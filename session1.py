821307fb368d4be910f8d1471b0c86909abed727
FIGHTER1230 <david.pregla@icloud.com>
2024-07-12 14:17:24 +0400

    Add files via upload
    My first session with MINA at Teach Me Code

---
+++ session1.py
@@ -0,0 +1,30 @@
+'''Name = input('Hello what is your name? ')
+print('nice to meet you',Name)
+LastName = input('OK! Whats your last name ')
+print(f'great {Name} now tell me your age mr {LastName}')
+'''
+'''x= 758943
+y= 39
+print(x+y)
+print(x*y)
+print(f'{x}+{y}={x+y}')
+print(f'{x}*{y}={x*y}')
+'''
+'''
+name= input('Hello what is your name? ')
+print(name.title())
+'''
+'''
+name = input('Hello what is your name? ').upper()
+print(name)
+'''
+'''
+num1=float(input('Number 1:'))
+num2=float(input('Number 2:'))
+print(f'{num1}*{num2}={num1 * num2}')
+print(f'{num1}/{num2}={num1 / num2}')
+print(round(num1 / num2))
+print(round(num1 / num2, 3))
+'''
+text= input('text:')
+print(text.replace(' ', '---').title())
